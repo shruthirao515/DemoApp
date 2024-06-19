8d82ddc7374ecd131e92cf0a0cffdefe0d90e44c
Shruthi Rao <shruthirao515@gmail.com>
2024-06-19 20:21:05 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by B NISHANT RAO on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
